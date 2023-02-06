3badb9dc8c33370b7e4847bba23349bb440f4d60
ManuRoch316 <113270946+ManuRoch316@users.noreply.github.com>
2023-02-05 20:27:08 -0600

    Add files via upload
    5 tipos de Datos con Punteros, Amperson &/*&

---
+++ Punteros_TiposDeDatos.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+	////////////////////////////////////////////////////////
+	//Borrar los /* */ de comentarios de cada tipo de DATO//
+	///////////////////////////////////////////////////////
+	
+	
+	/*Puntero1 LONG
+	long numero = 12345678910;
+	size_t tamanio = sizeof(numero);
+	cout<<"Valor de la variable: "<<numero<<endl;
+	cout<<"Tamaño de la variable: "<<tamanio<<" BYTES"<<endl;
+	cout<<"BITS: "<<tamanio*8<<"BITS"<<endl;
+	
+	numero = 22334455667;
+	cout<<"Valor modificado: "<<numero<<endl;
+	cout<<"Dirección de memoria: "<<&numero<<endl;
+	
+	cout<<"Valor obtenido con *&(apuntador): "<<*&numero<<endl<<endl;
+	*/
+	
+	
+	/*Puntero2 SHORT
+	short pequeno = 3;
+	size_t tamanio2 = sizeof(pequeno);
+	cout<<"\nValor de la variable: "<<pequeno<<endl;
+	cout<<"Tamaño de la variable: "<<tamanio2<<" BYTES"<<endl;
+	cout<<"BITS: "<<tamanio2*8<<"BITS"<<endl;
+	
+	pequeno = 7;
+	cout<<"Valor modificado: "<<pequeno<<endl;
+	cout<<"Dirección de memoria: "<<&pequeno<<endl;
+	
+	cout<<"Valor obtenido con *&(apuntador): "<<*&pequeno;
+	*/
+	
+	
+	
+	/*Puntero 3 LONG DOUBLE
+	long double grande = 123456789123456789;
+	size_t tamanio3 = sizeof(grande);
+	cout<<"\nValor de la variable: "<<grande<<endl;
+	cout<<"Tamaño de la variable: "<<tamanio3<<" BYTES"<<endl;
+	cout<<"BITS: "<<tamanio3*8<<"BITS"<<endl;
+	
+	grande = 231453674532456789;
+	cout<<"Valor modificado: "<<grande<<endl;
+	cout<<"Dirección de memoria: "<<&grande<<endl;
+	
+	cout<<"Valor obtenido con *&(apuntador): "<<*&grande;
+	*/
+	
+	
+	
+	/*Puntero 4 BOOL
+	bool casado = false;
+	size_t tamanio4 = sizeof(casado);
+	cout<<"Valor de la variable: "<<casado<<endl;
+	cout<<"Tamaño de la variable: "<<tamanio4<<" BYTES"<<endl;
+	cout<<"BITS: "<<tamanio4*8<<"BITS"<<endl;
+	
+	casado = true;
+	cout<<"Valor modificado: "<<casado<<endl;
+	cout<<"Dirección de memoria: "<<&casado<<endl;
+	
+	cout<<"Valor obtenido con *&(apuntador): "<<*&casado;
+	*/
+	
+	
+	
+	/*Puntero 5 CHAR
+	char caracter = 'p';
+	size_t tamanio5 = sizeof(caracter);
+	cout<<"\nValor de la variable: "<<caracter<<endl;
+	cout<<"Tamaño de la variable: "<<tamanio5<<" BYTES"<<endl;
+	cout<<"BITS: "<<tamanio5*8<<"BITS"<<endl;
+	
+	caracter = 'a';
+	cout<<"\nValor modificado: "<<caracter<<endl;
+	cout<<"Dirección de memoria: "<<&caracter<<endl;
+	
+	cout<<"Valor obtenido con *&(apuntador): "<<*&caracter;
+	*/
+}
