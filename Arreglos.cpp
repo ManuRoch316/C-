11f5e885542878a11fa35abfb44673d231fa5339
ManuRoch316 <113270946+ManuRoch316@users.noreply.github.com>
2023-02-06 23:17:37 -0600

    Add files via upload

---
+++ Arreglos.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+	char letras[5];
+	char* puntero;
+	
+	puntero = &letras[0];
+	for(int i=0; i<5; i++){
+		cout<<"Ingresa letra: ";
+		cin>>letras[i];
+	}
+	
+	cout<<"\nDATOS DE ARREGLO"<<endl;
+	for(int i=0; i<=4;i++){
+		cout<<"Letras ["<<*(puntero+i)<<"]\n";
+	}
+}
